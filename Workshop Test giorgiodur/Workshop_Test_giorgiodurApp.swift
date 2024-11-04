23d3143a58cc3ef120a6a1026e227d6635692d1e
Giorgio Durante <gorgo.durante@gmail.com>
2024-11-04 10:34:12 +0100

    Initial Commit

---
+++ Workshop Test giorgiodur/Workshop_Test_giorgiodurApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_Test_giorgiodurApp.swift
+//  Workshop Test giorgiodur
+//
+//  Created by Giorgio Durante on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_Test_giorgiodurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
