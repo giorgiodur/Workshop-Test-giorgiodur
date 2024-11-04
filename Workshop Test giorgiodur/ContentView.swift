94e7fcc799a62eaf0278f13f4d48a8cabbfbb4da
Paola Campanile <paolacampanile799@gmail.com>
2024-11-04 11:34:43 +0100

    Changed the icon and the text

--- Workshop Test giorgiodur/ContentView.swift
+++ Workshop Test giorgiodur/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "applelogo")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Ooooooo, world!")
+            Text("Rotten Apples")
         }
         .padding()
         
