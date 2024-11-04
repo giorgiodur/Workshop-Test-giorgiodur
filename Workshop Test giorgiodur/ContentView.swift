cde7c836042d1c1bd45bc3a3052d43de3ac8929d
Giorgio Durante <gorgo.durante@gmail.com>
2024-11-04 10:55:50 +0100

    kjhkhjgiugiyu

--- Workshop Test giorgiodur/ContentView.swift
+++ Workshop Test giorgiodur/ContentView.swift
@@ -13,9 +13,11 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Ooooooo, world!")
         }
         .padding()
+        
+        
     }
 }
 
